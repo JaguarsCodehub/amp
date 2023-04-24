584f8d5123c076e54a347a956a585efb9eb3066d
bhuk21 <131268254+bhuk21@users.noreply.github.com>
2023-04-24 20:19:16 +0530

    Add files via upload

---
+++ 6a - Creation of Menu/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.practical_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater mi=getMenuInflater();
+        mi.inflate(R.menu.menus,menu);
+        return true;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.newb:
+                Toast.makeText(this, "File Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.Share:
+                Toast.makeText(this, "Open Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.delete:
+                Toast.makeText(this, "Save Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.save:
+                Toast.makeText(this, "Save As Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.search:
+                Toast.makeText(this, "Delete Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.Exit:
+                Toast.makeText(this, "Exit Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                Toast.makeText(this, "Default", Toast.LENGTH_SHORT).show();
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
